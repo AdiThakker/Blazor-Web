2a08625d4c1ab72bc483681e3ff3ebf5e6abe67e
Aditya D Thakker <aditya.thakker@gmail.com>
2020-06-22 08:14:21 -0400

    Add project files.

---
+++ Blazor-Web1/Server/Hubs/ClientHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorWeb.Server.Services;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorWeb.Server.Hubs
+{
+    public class ClientHub : Hub
+    {
+        private readonly TimerService _timerService;
+
+        public ClientHub(TimerService timerService) => _timerService = timerService;
+
+        public async Task Start() => await Task.Run(() => _timerService.Start());
+
+        public async Task Stop() => await Task.Run(() => _timerService.Stop());
+    }
+}
---
+++ Blazor-Web1/Server/Services/TimerService.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using BlazorWeb.Server.Hubs;
+
+namespace BlazorWeb.Server.Services
+{
+    public class TimerService
+    {
+        private Timer _timer;
+        private IHubContext<ClientHub> _hub;
+
+        public TimerService(IHubContext<ClientHub> hub)
+        {
+            if (_timer == null)
+            {
+                _timer = new Timer(1000);
+                _timer.Elapsed += Timer_Elapsed;
+                _hub = hub;
+            }
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (_hub != null)
+                _hub.Clients.All.SendAsync("ReceiveTime", DateTime.Now.ToString());
+        }
+
+        public void Start() => _timer.Start();
+
+        public void Stop()
+        {
+            _timer.Stop();
+
+            if (_hub != null)
+                _hub.Clients.All.SendAsync("StopTime");
+        }
+    }
+}
